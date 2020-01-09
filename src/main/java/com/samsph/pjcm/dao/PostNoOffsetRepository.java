015727223ee1bb2ccf083a0f4923dbd958f3eb9c
weekdawn <571426779@qq.com>
2020-01-09 15:02:50 +0800

    new

--- src/main/java/com/samsph/pjcm/dao/PostNoOffsetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.samsph.pjcm.dao;
-
-import com.samsph.pjcm.model.Material;
-import com.samsph.pjcm.model.PostNoOffset;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostNoOffsetRepository extends JpaRepository<PostNoOffset, Integer> {
-}
